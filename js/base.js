a14843ddafc4b4ebffa7ccdb72c4ffbb5ee89d12
Mr zh <877854886@qq.com>
2019-01-11 14:18:10 +0800

    test

--- js/base.js
+++ js/base.js
@@ -1,5 +1,7 @@
 window.base={
-    g_restUrl:'https://www.solelycloud.com/api/public/index.php/api/v1/',
+    g_restUrl:'https://xianjiaoda.solelycloud.com/api/public/index.php/api/v1/',
+
+
     getData:function(params){
         if(!params.type){
             params.type='get';
@@ -9,13 +11,33 @@ window.base={
             type:params.type,
             url:this.g_restUrl+params.url,
             data:params.data,
-            beforeSend: function (XMLHttpRequest) {
-                if (params.tokenFlag) {
-                    XMLHttpRequest.setRequestHeader('token', that.getLocalStorage('token'));
-                }
-            },
             success:function(res){
-                params.sCallback && params.sCallback(res);
+                if(res.solely_code==201000){
+                    var loca = window.location;
+                    window.location.href = loca.origin + loca.pathname;
+                }else if(res.solely_code==200000){
+                /*	console.log(that.GetUrlRelativePath().substr(6,5));
+                    if(that.GetUrlRelativePath().substr(5,4)=='user'||that.GetUrlRelativePath().substr(5,5)=='index'){
+                        localStorage.removeItem('user_token');
+                        localStorage.removeItem('user_no');
+                        that.getUserToken();
+                    }else if(that.GetUrlRelativePath().substr(5,8)=='merchant'){
+                        localStorage.removeItem('merchant_token');
+                        localStorage.removeItem('merchant_no');
+                        window.location.href = './login.html'
+                    };*/
+                    if(localStorage.getItem('user_type')==0){
+                    	window.location.href = './login.html'	
+                    }else if(localStorage.getItem('user_type')==1){
+                    	window.location.href = './manager_login.html'
+                    }
+                    localStorage.removeItem('token');
+                    localStorage.removeItem('user_no');
+                    localStorage.removeItem('user_type');
+                    
+                }else{
+                    params.sCallback && params.sCallback(res);
+                };
             },
             error:function(res){
                 params.eCallback && params.eCallback(res);
@@ -49,6 +71,80 @@ window.base={
         this.getData(allParams)
     },
 
+    userGet:function(param,callback) {
+  
+        var allParams = {
+            url:'Base/User/get',
+            type:'post',
+            data:param,
+            sCallback: function(data){
+                callback&&callback(data);
+            }
+        };
+        this.getData(allParams)
+    },
+    relationGet(param,callback){
+        var allParams ={
+            url:'Common/Relation/get',
+            type:'post',
+            data:param,
+            sCallback: function(data) {
+                callback && callback(data);
+            }
+        };
+        this.getData(allParams);       
+    },
+
+
+    relationAdd(param,callback){
+        var allParams ={
+            url:'Common/Relation/add',
+            type:'post',
+            data:param,
+            sCallback: function(data) {
+                callback && callback(data);
+            }
+        };
+        this.getData(allParams);       
+    },
+
+    relationUpdate(param,callback){
+        var allParams ={
+            url:'Common/Relation/update',
+            type:'post',
+            data:param,
+            sCallback: function(data) {
+                callback && callback(data);
+            }
+        };
+        this.getData(allParams);       
+    },
+
+    relationDel(param,callback){
+        var allParams ={
+            url:'Common/Relation/delete',
+            type:'post',
+            data:param,
+            sCallback: function(data) {
+                callback && callback(data);
+            }
+        };
+        this.getData(allParams);       
+    },
+
+    userInfoUpdate:function(param,callback) {
+  
+        var allParams = {
+            url:'Common/UserInfo/update',
+            type:'post',
+            data:param,
+            sCallback: function(data){
+                callback&&callback(data);
+            }
+        };
+        this.getData(allParams)
+    },
+
     messageAdd:function(param,callback) {
   
         var allParams = {
@@ -62,10 +158,58 @@ window.base={
         this.getData(allParams)
     },
 
+    messageGet:function(param,callback) {
+  
+        var allParams = {
+            url:'Common/Message/get',
+            type:'post',
+            data:param,
+            sCallback: function(data){
+                callback&&callback(data);
+            }
+        };
+        this.getData(allParams)
+    },
+
     messageUpdate:function(param,callback) {
   
         var allParams = {
             url:'Common/Message/update',
+            type:'post',
+            data:param,
+            sCallback: function(data){
+                callback&&callback(data);
+            }
+        };
+        this.getData(allParams)
+    },
+
+    getMemberToken:function(callback){
+        var href =  window.location.href;
+        console.log('href',href);
+        var token = localStorage.getItem('member_token');
+        if(token){
+           callback&&callback();
+        }else{
+            window.location.href = './login.html'
+        };
+    },
+
+    getManergerToken:function(callback){
+        var href =  window.location.href;
+        console.log('href',href);
+        var token = localStorage.getItem('manerger_token');
+        if(token){
+           callback&&callback();
+        }else{
+            window.location.href = './login.html'
+        };
+    },
+
+    login:function(param,callback) {
+
+        var allParams = {
+            url:'Func/Common/loginByUp',
             type:'post',
             data:param,
             sCallback: function(data){
@@ -164,7 +308,7 @@ window.base={
                 alert('已经到底啦')
             }else{
                 self.paginate.currentPage++;
-                self.getMainData();
+                self.getMainData(true);
             }
         };
         if(dataSet.type == 'back'){
@@ -172,7 +316,7 @@ window.base={
                 alert('已经没有啦')
             }else{
                 self.paginate.currentPage--;
-                self.getMainData();
+                self.getMainData(true);
             }
         };
          
@@ -184,12 +328,49 @@ window.base={
             self.linkPage = '';
         }else{
             self.paginate.currentPage = self.linkPage;
-            self.getMainData();
+            self.getMainData(true);
         }
          
     },
 
 
+    upLoadFile:function(param,callback) {
+        var that=this;
+        $.ajax({ // $.post，告辞
+            type: 'post',
+            contentType: false, // 关关关！必须得 false
+                                // 这个不关会扔一个默认值 application/x-www-form-urlencoded 过去，后端拿不到数据的！
+                                // 而且你甚至不能传个字符串 'multipart/form-data'，后端一样拿不到数据！
+            processData: false, // 关关关！重点
+            url: 'https://xianjiaoda.solelycloud.com/api/public/index.php/api/v1/Base/FtpFile/upload',
+            data: param,
+            success:function(res){
+
+                if(res.solely_code==201000){
+                    var loca = window.location;
+                    window.location.href = loca.origin + loca.pathname;
+                }else if(res.solely_code==200000){
+                    localStorage.removeItem('token');
+                    localStorage.removeItem('user_no');
+                    localStorage.removeItem('user_type');
+                    window.location.href = './login.html'
+                }else{
+                    callback && callback(res);
+                };
+
+            },
+            error:function(res){
+                callback && callback(res);
+            }
+        });
+    },
+
+	shortString:function(str,i){
+	   if(str){
+	     return str.substring(0,i);
+	   };
+	  console.log('str',666)
+	},
 
 }
 
